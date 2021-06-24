3a75a6352e2f7daa760d248298be3b67cac5c178
Lucas Cruz <lucascs20182@gmail.com>
2021-06-24 10:58:54 -0300

    adicione os demais campos requisitos no cadastro

--- exemplo07-testes-requisicao/src/pages/Cadastro.jsx
+++ exemplo07-testes-requisicao/src/pages/Cadastro.jsx
@@ -10,17 +10,24 @@ const Cadastro = () => {
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [senha, setSenha] = useState("");
+    const [nome, setNome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [cep, setCep] = useState("");
+    const [numero, setNumero] = useState("");
+    const [complemento, setComplemento] = useState("");
 
     const criarUsuario = (e) => {
         e.preventDefault();
 
-        if(!email || !username || !senha){
-            alert("Favor informar e-mail, username e senha");
-            return;
+        if(!email || !username || !senha || !nome || !cpf || !cep
+            || !numero){
+                alert("Complemento é opcional. Favor, informar todos os demais campos");
+                return;
         }
 
-        apiUsuario.cadastrar(email, username, senha)
+        apiUsuario.cadastrar(email, username, senha, nome, cpf, cep, numero, complemento)
             .then(resposta => {
+                console.log(resposta)
                 alert("Usuário cadastrado!")
                 window.open("/login", "_self")
             })
@@ -35,7 +42,7 @@ const Cadastro = () => {
             <div className="campo">
                 <label htmlFor="email">E-mail: </label>
                 
-                <input type="email" id="email" value={email}
+                <input required type="email" id="email" value={email}
                     onChange={e => setEmail(e.target.value)}
                     placeholder="exemplo@exemplo.com" />
             </div>
@@ -43,7 +50,7 @@ const Cadastro = () => {
             <div className="campo">
                 <label htmlFor="username">Username: </label>
                 
-                <input type="text" id="username" value={username}
+                <input required type="text" id="username" value={username}
                     onChange={e => setUsername(e.target.value)}
                     placeholder="karine" />
             </div>
@@ -51,12 +58,52 @@ const Cadastro = () => {
             <div className="campo">
                 <label htmlFor="senha">Senha: </label>
                 
-                <input type="password" id="senha" value={senha}
+                <input required type="password" id="senha" value={senha}
                     onChange={e => setSenha(e.target.value)}
                     placeholder="123456" />
             </div>
 
-            <button onClick={criarUsuario}>Entrar</button>
+            <div className="campo">
+                <label htmlFor="nome">Nome: </label>
+                
+                <input required type="text" id="nome" value={nome}
+                    onChange={e => setNome(e.target.value)}
+                    placeholder="Karine Alves" />
+            </div>
+
+            <div className="campo">
+                <label htmlFor="cpf">CPF: </label>
+                
+                <input required type="text" id="cpf" value={cpf}
+                    onChange={e => setCpf(e.target.value)}
+                    placeholder="12632129190" />
+            </div>
+
+            <div className="campo">
+                <label htmlFor="cep">CEP: </label>
+                
+                <input required type="text" id="cep" value={cep}
+                    onChange={e => setCep(e.target.value)}
+                    placeholder="25780000" />
+            </div>
+
+            <div className="campo">
+                <label htmlFor="numero">Número da casa: </label>
+                
+                <input required type="text" id="numero" value={numero}
+                    onChange={e => setNumero(e.target.value)}
+                    placeholder="281" />
+            </div>
+
+            <div className="campo">
+                <label htmlFor="complemento">Complemento: </label>
+                
+                <input required type="text" id="complemento" value={complemento}
+                    onChange={e => setComplemento(e.target.value)}
+                    placeholder="sei la" />
+            </div>
+
+            <button onClick={criarUsuario}>Cadastrar</button>
 
             <Link to={'/login'}>Fazer login</Link>
         </form>
--- exemplo07-testes-requisicao/src/services/usuario-api.js
+++ exemplo07-testes-requisicao/src/services/usuario-api.js
@@ -1,8 +1,16 @@
 import api from './api';
 
-function cadastrar(email, username, senha) {
+function cadastrar(email, username, senha, nome, cpf, cep, numero, complemento) {
+    const produto = {
+        email, username, senha, nome, cpf, 
+        
+        enderecosDoCliente: [{
+            cep, numero, complemento
+        }]
+    }
+
     return new Promise((resolve, reject) => {
-        return api.post('/create', { email, username, senha })
+        return api.post('/create', produto)
             .then(response => resolve(response))
             .catch(error => reject(error))
     });
