be79a144c87a4281df2a7add951e4d4c48818ce7
Lucas Cruz <lucascs20182@gmail.com>
2021-07-10 22:19:40 -0300

    suba experimento com controlled component

--- react-experiments-v2/src/App.js
+++ react-experiments-v2/src/App.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 
+// exemplo de controlled component
 export default function App(props) {
-  const [usuario, setUsuario] = useState('Guilherme');
+  const [user, setUser] = useState('');
 
   return (
     <>
-      <h1>{`${props.boasVindas}, ${usuario || props.user}!`}</h1>
+      <p>{user}</p>
 
       <label htmlFor="username">Username:</label>
-      <input type="text" id="username" className="usernameInput"
-        placeholder="ex.: lucascs20182" />
+      <input type="text" placeholder="ex.: lucascs20182" value={user}
+        className="usernameInput" onChange={e => setUser(e.target.value)} />
       
       <button>Pesquisar</button>
     </>
