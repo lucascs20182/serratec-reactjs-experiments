7a4160e7daca9de6ae60289dceb3f6c3c2e1a99b
Lucas Cruz <lucascs20182@gmail.com>
2021-07-11 09:30:46 -0300

    adicione loading enquanto a promise não resolve

--- react-experiments-v2/src/pages/Home/index.jsx
+++ react-experiments-v2/src/pages/Home/index.jsx
@@ -3,17 +3,28 @@ import axios from 'axios';
 
 import { Container, Input, Button } from './styled';
 
+import loadingImg from '../../assets/loading.gif';
+
 export default function Home() {
   const [usuario, setUsuario] = useState('');
   const [situacaoPesquisa, setSituacaoPesquisa] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   function handlePesquisa() {
+    setLoading(true);
+
     axios.get(`https://api.github.com/users/${usuario}/repos`)
       .then(res => {
         console.log(res);
         setSituacaoPesquisa(true);
+
+        setLoading(false);
       })
-      .catch(err => setSituacaoPesquisa(false));
+      .catch(err => {
+        setSituacaoPesquisa(false);
+
+        setLoading(false);
+      });
   }
 
   return (
@@ -24,6 +35,8 @@ export default function Home() {
         
         <Button onClick={handlePesquisa}>Pesquisar</Button>
       </div>
+
+      {loading ? <img src={loadingImg} alt="loading..." /> : ''}
 
       {situacaoPesquisa === false ? <p>Usuário não encontrado. Tente novamente.</p> : ''}
     </Container>
